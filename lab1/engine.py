03e2209e60b06f32bd0e9a5308d575623d903490
BaldiSlayer <lisov.a2005@yandex.ru>
2024-02-25 19:17:55 +0300

    Fix code style

--- lab1/engine.py
+++ lab1/engine.py
@@ -132,11 +132,11 @@ class Window(Drawable):
 
 
 class Rectangle(Drawable):
-    x1 = 0
-    y1 = 0
-    x2 = 0
-    y2 = 0
-    color = []
+    x1: int
+    y1: int
+    x2: int
+    y2: int
+    color: list
 
     def __init__(self, x1, y1, x2, y2, color):
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
